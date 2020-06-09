68ba29c22bd81e172d368d8331f5a5de24bfb0b8
snehalp29 <snehalpatkar@Gmail.com>
2020-06-09 09:21:09 -0400

    added polifills updated ngx-build

--- migrate-components/src/app/app.module.ts
+++ migrate-components/src/app/app.module.ts
@@ -1,18 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 
 import { AppComponent } from './app.component';
 import { AdminLoginComponent } from './admin-login/admin-login.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    AdminLoginComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AdminLoginComponent],
 })
-export class AppModule { }
+export class AppModule {
+  constructor(public injector: Injector) {}
+
+  ngDoBootstrap() {
+    const AdminLoginElement = createCustomElement(AdminLoginComponent, {
+      injector: this.injector,
+    });
+
+    customElements.define('admin-login-el', AdminLoginElement);
+  }
+}
