193ac45405c76f85806b836ac44f498d31ea9192
snehalp29 <snehalpatkar@Gmail.com>
2020-06-09 14:30:33 -0400

    created component

--- migrate-components/src/app/admin-login/admin-login.component.ts
+++ migrate-components/src/app/admin-login/admin-login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AdminModel } from './admin-model';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NgAuthService } from '../security/ng-auth.service';
 @Component({
   selector: 'app-admin-login',
   templateUrl: './admin-login.component.html',
-  styleUrls: ['./admin-login.component.css']
+  styleUrls: ['./admin-login.component.css'],
 })
 export class AdminLoginComponent implements OnInit {
+  model: AdminModel = new AdminModel();
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private ngAuthService: NgAuthService) {}
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: [''],
+      password: [''],
+    });
   }
 
+  login() {
+    this.ngAuthService.login(this.loginForm.value).subscribe(() => {
+      () => {
+        console.log('user authenticated');
+      };
+    });
+  }
 }
