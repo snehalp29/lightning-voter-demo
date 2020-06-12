d7508ae47d592bc2d7baec34029635261f9050cc
snehalp29 <snehalpatkar@Gmail.com>
2020-06-12 17:36:11 -0400

    Added Users

--- server/database/users.js
+++ server/database/users.js
@@ -1,6 +1,34 @@
 var users = [
-  {id: 1, email: 'joe@joe.com', password: 'pass', firstName: 'Joe', lastName: 'Eames', isAdmin: true},
-  {id: 2, email: 'bob@bob.com', password: 'pass', firstName: 'Bob', lastName: 'Smith'}
-]
+  {
+    id: 1,
+    email: 'joe@joe.com',
+    password: 'pass',
+    firstName: 'Joe',
+    lastName: 'Eames',
+    isAdmin: true,
+  },
+  {
+    id: 1,
+    email: 's@p.com',
+    password: 'pass',
+    firstName: 'Snehal',
+    lastName: 'Patkar',
+    isAdmin: true,
+  },
+  {
+    id: 1,
+    email: 't@p.com',
+    password: 'pass',
+    firstName: 'Tanish',
+    lastName: 'Patkar',
+  },
+  {
+    id: 2,
+    email: 'bob@bob.com',
+    password: 'pass',
+    firstName: 'Bob',
+    lastName: 'Smith',
+  },
+];
 
 module.exports = users;
