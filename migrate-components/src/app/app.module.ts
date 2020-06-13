60472f565ab42794050b62e91a75ddfc4cedb230
snehalp29 <snehalpatkar@Gmail.com>
2020-06-13 15:38:36 -0400

    imported component and updated for logic

--- migrate-components/src/app/app.module.ts
+++ migrate-components/src/app/app.module.ts
@@ -8,12 +8,21 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { RouterModule } from '@angular/router';
 import { APP_BASE_HREF } from '@angular/common';
+import {
+  ResultsModule,
+  ResultsComponent,
+  SessionDetailWithVotesComponent,
+  SessionDetailComponent,
+} from './results';
+import { SharedModule, DetailPanelComponent } from './shared';
 
 @NgModule({
   declarations: [AppComponent, AdminLoginComponent],
   imports: [
     BrowserModule,
     ReactiveFormsModule,
+    SharedModule,
+    ResultsModule,
     HttpClientModule,
     RouterModule.forRoot([]),
   ],
@@ -25,10 +34,20 @@ export class AppModule {
   constructor(public injector: Injector) {}
 
   ngDoBootstrap() {
-    const AdminLoginElement = createCustomElement(AdminLoginComponent, {
-      injector: this.injector,
-    });
+    const elements: any[] = [
+      [AdminLoginComponent, 'admin-login-el'],
+      [ResultsComponent, 'results-el'],
+      [SessionDetailWithVotesComponent, 'session-detail-with-votes-el'],
+      [SessionDetailComponent, 'session-detail-el'],
+      [DetailPanelComponent, 'detail-panel-el'],
+    ];
 
-    customElements.define('admin-login-el', AdminLoginElement);
+    for (const [component, name] of elements) {
+      const el = createCustomElement(component, {
+        injector: this.injector,
+      });
+
+      customElements.define(name, el);
+    }
   }
 }
