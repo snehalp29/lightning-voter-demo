e82250f9549c78064402f37f0f81c425ba6f96ff
snehalp29 <snehalpatkar@Gmail.com>
2020-06-14 18:58:22 -0400

    added directive

--- migrate-components/src/app/shared/shared.module.ts
+++ migrate-components/src/app/shared/shared.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { TalkDurationPipe } from './talk-duration.pipe';
 import { DetailPanelComponent } from './detail-panel/detail-panel.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ZoomInDirective } from './zoom-in.directive';
 
 @NgModule({
-  declarations: [TalkDurationPipe, DetailPanelComponent],
+  declarations: [TalkDurationPipe, DetailPanelComponent, ZoomInDirective],
   imports: [CommonModule],
   exports: [TalkDurationPipe, DetailPanelComponent, ReactiveFormsModule],
-  entryComponents: [TalkDurationPipe, DetailPanelComponent],
+  entryComponents: [TalkDurationPipe, DetailPanelComponent, ZoomInDirective],
 })
 export class SharedModule {}
---
+++ migrate-components/src/app/shared/zoom-in.directive.spec.ts
@@ -0,0 +1,8 @@
+import { ZoomInDirective } from './zoom-in.directive';
+
+describe('ZoomInDirective', () => {
+  it('should create an instance', () => {
+    const directive = new ZoomInDirective();
+    expect(directive).toBeTruthy();
+  });
+});
